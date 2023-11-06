c0998c6c5438f08d6fd45f6d4bb62fe9830ec060
darklink2003 <108371170+darklink2003@users.noreply.github.com>
2023-11-05 20:53:43 -0500

    vs6

--- python/funciones.py
+++ python/funciones.py
@@ -1,3 +1,24 @@
+#-----------------------------Estructura------------------------------------#
+texto=""
+url=""
+
+def crear_enlace(texto, url):
+  """
+  Crea una estructura HTML con un enlace.
+
+  Args:
+    texto: El texto que se mostrará en el enlace.
+    url: La URL a la que apunta el enlace.
+    target: La pestaña en la que se abrirá el enlace.
+
+  Returns:
+    El código HTML del enlace.
+  """
+
+  return """<a href="{url}" >{texto}</a>""".format(
+      texto=texto, url=url
+  )
+
 #-------------guardar_frase----------------------#
 
 def guardar_frase(frase, nombre_archivo):
@@ -65,6 +86,32 @@ def Intercambiar_textos():
 
     print("El contenido de los archivos se ha intercambiado correctamente.")
     
+
+
+#-------------crea_archivo_que_contega_un_enlace----------------------#
+def crea_enlace():
+    # Solicitamos al usuario que ingrese el nombre del archivo HTML.
+    nombre_archivo_html = input("Ingresa el nombre del archivo HTML: ")
+
+    # Solicitamos al usuario que ingrese el texto para el enlace.
+    texto = input("Ingresa el texto para el enlace: ")
+
+    # Solicitamos al usuario que ingrese la URL a la que apuntará el enlace.
+    url = input("Ingresa la URL a la que apuntará el enlace: ")
+
+   
+
+    # Creamos el enlace HTML utilizando la función crear_enlace().
+    enlace_html = crear_enlace(texto, url)
+
+    # Creamos el contenido HTML que contiene el enlace.
+    contenido_html = f"<html><body>{enlace_html}</body></html>"
+
+    # Guardamos el contenido HTML en el archivo HTML especificado.
+    guardar_frase(contenido_html, nombre_archivo_html)
+
+    print(f"El archivo HTML '{nombre_archivo_html}' se ha creado con el enlace HTML.")
+
 #-------------menu----------------------#
 
 def menu():
@@ -80,13 +127,15 @@ def menu():
     print("2. Leer archivo")
     print("3. Acerca de ")
     print("4. Intercambiar ")
+    print("5. Crear una página ")
+
     print("0. Salir")
 
     # Solicitamos la opción al usuario.
     opc = input("Introduce una opción: ")
 
     # Validamos la opción del usuario.
-    while opc not in ["0", "1", "2","3","4"]:
+    while opc not in ["0", "1", "2","3","4","5"]:
         print("Opción no válida.")
         opc = input("Introduce una opción: ")
 
@@ -104,20 +153,24 @@ def menu():
         print(contenido)
         menu()
     elif opc=="3":
-        #acerca de 
-        print("Nombre: Programa de edicion ")
-        print("Version: 4")
-        print("Autor:Pablo Mondragon ")
+        # Acerca de 
+        print("Nombre: Programa de edición ")
+        print("Versión: 4")
+        print("Autor: Pablo Mondragón ")
         menu()
     elif opc == "4":
         # Intercambiamos los textos.
         Intercambiar_textos()
+        menu()
+    elif opc == "5":
+        # Creamos una página con un enlace.
+        crea_enlace()
         menu()
         
     else:
         # Salimos del programa.
         print("Gracias por usar el menú.")
 
-#-------------despligue----------------------#
+#-------------despliegue----------------------#
 
 menu()
